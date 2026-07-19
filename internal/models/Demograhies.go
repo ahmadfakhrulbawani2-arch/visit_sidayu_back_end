2d9f3bf3fce44018ab46cba39b4f05686914268f
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2026-07-19 10:58:02 +0700

    feat: add geografi and demografi models

---
+++ internal/models/Demograhies.go
@@ -0,0 +1,71 @@
+package models
+
+type Demographies struct {
+	BaseModel
+	VillageName           string `json:"village_name" gorm:"unique;not null"`
+	Demography_Data_Year  int    `json:"demography_data_year" gorm:"not null"`
+	MalePopulation        int    `json:"male_population"`
+	FemalePopulation      int    `json:"female_population"`
+	TotalPopulation       int    `json:"total_population"`
+	PopulationDensityUnit string `json:"population_density_unit" gorm:"not null"`
+	FamiliesNumber        int    `json:"families_number"`
+	NumberOfBirth         int    `json:"number_of_birth"`
+	NumberOfDeath         int    `json:"number_of_death"`
+	// BirthDeathYearData     int    `json:"birth_death_year_data"` // in which year the data is audited
+	WorkingPopulation      int `json:" working_population"`
+	UnemployedPopulation   int `json:"unemployed_population"`
+	HousekeepingPopulation int `json:"housekeeping_population"`
+	StudentPopulation      int `json:"student_population"`
+	// RoleGroupYearData      int    `json:"role_group_year_data"` // in which year the data is audited
+	SourceName         string `json:"source_name" gorm:"not null"`
+	ExternalLinkSource string `json:"external_link_source" gorm:"not null"`
+}
+
+type CreateDemographies struct {
+	VillageName          string `json:"village_name" binding:"required"`
+	Demography_Data_Year int    `json:"demography_data_year" gorm:"not null"`
+	MalePopulation       int    `json:"male_population"`
+	FemalePopulation     int    `json:"female_population"`
+	TotalPopulation      int    `json:"total_population"`
+	FamiliesNumber       int    `json:"families_number"`
+	NumberOfBirth        int    `json:"number_of_birth"`
+	NumberOfDeath        int    `json:"number_of_death"`
+	// BirthDeathYearData     int    `json:"birth_death_year_data"` // in which year the data is audited
+	WorkingPopulation      int `json:" working_population"`
+	UnemployedPopulation   int `json:"unemployed_population"`
+	HousekeepingPopulation int `json:"housekeeping_population"`
+	StudentPopulation      int `json:"student_population"`
+	// RoleGroupYearData      int    `json:"role_group_year_data"` // in which year the data is audited
+	SourceName         string `json:"source_name" binding:"required"`
+	ExternalLinkSource string `json:"external_link_source" binding:"required"`
+}
+
+type GetDemographies struct {
+	BaseModel
+	VillageName          string `json:"village_name"`
+	Demography_Data_Year int    `json:"demography_data_year"`
+	MalePopulation       int    `json:"male_population,omitempty"`
+	FemalePopulation     int    `json:"female_population,omitempty"`
+	TotalPopulation      int    `json:"total_population,omitempty"`
+	FamiliesNumber       int    `json:"families_number,omitempty"`
+	NumberOfBirth        int    `json:"number_of_birth,omitempty"`
+	NumberOfDeath        int    `json:"number_of_death,omitempty"`
+	// BirthDeathYearData     int    `json:"birth_death_year_data"` // in which year the data is audited
+	WorkingPopulation      int `json:" working_population,omitempty"`
+	UnemployedPopulation   int `json:"unemployed_population,omitempty"`
+	HousekeepingPopulation int `json:"housekeeping_population,omitempty"`
+	StudentPopulation      int `json:"student_population,omitempty"`
+	// RoleGroupYearData      int    `json:"role_group_year_data"` // in which year the data is audited
+	SourceName         string `json:"source_name"`
+	ExternalLinkSource string `json:"external_link_source"`
+}
+
+type GetDistrictDemographies struct {
+	Demography_Data_Year  int      `json:"demography_data_year"`
+	MalePopulation        int      `json:"male_population,omitempty"`
+	FemalePopulation      int      `json:"female_population,omitempty"`
+	TotalPopulation       int      `json:"total_population,omitempty"`
+	PopulationDensity     int      `json:"population_density"`
+	PopulationDensityUnit string   `json:"population_density_unit"`
+	Sources               []string `json:"sources"`
+}
