de8186a35c31dd3236765e57315cc13de88e9087
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2026-07-19 14:15:32 +0700

    feat: add blogs model

---
+++ internal/models/Blogs.go
@@ -0,0 +1,37 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Blogs struct {
+	BaseModel
+	Title                string     `json:"title" gorm:"not null"`
+	Description          string     `json:"description,omitempty"`
+	Tags                 []string   `json:"tags" gorm:"type:text[];not null"`
+	Content              string     `json:"content" gorm:"not null"`
+	Author               string     `json:"author" gorm:"not null"`
+	BlogReleaseDatetime  time.Time  `json:"blog_release_datetime" gorm:"not null"`
+	EstimatedMinutesRead int        `json:"estimated_minutes_read" gorm:"not null"`
+	ThumbnailID          *uuid.UUID `json:"thumbnail_id,omitempty"`
+	Thumbnail            *Images    `json:"thumbnail,omitempty" gorm:"foreignKey:ThumbnailID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;"`
+	Location             string     `json:"location,omitempty"`
+	ExternalLinks        []string   `json:"external_links,omitempty" gorm:"type:text[]"`
+	Timeline             *Timelines `json:"timeline,omitempty" gorm:"foreignKey:BlogID;references:ID;"`
+}
+
+type CreateBlogs struct {
+	Title                string           `json:"title" binding:"required"`
+	Description          string           `json:"description"`
+	Tags                 []string         `json:"tags" binding:"required"`
+	Content              string           `json:"content" binding:"required"`
+	Author               string           `json:"author" binding:"required"`
+	BlogReleaseDatetime  time.Time        `json:"blog_release_datetime" binding:"required"`
+	EstimatedMinutesRead int              `json:"estimated_minutes_read" binding:"required"`
+	ThumbnailID          *uuid.UUID       `json:"thumbnail_id"`
+	Location             string           `json:"location"`
+	ExternalLinks        []string         `json:"external_links"`
+	Timeline             *CreateTimelines `json:"timeline"`
+}
