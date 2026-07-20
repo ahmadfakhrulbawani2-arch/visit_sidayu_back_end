f4c8840484cec41a50da31bf70a5cb49bf825df9
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2026-07-20 12:19:29 +0700

    fix: migration issue

--- db/migrations/migration.go
+++ db/migrations/migration.go
@@ -2,26 +2,11 @@ package migrations
 
 import (
 	"log"
+	"simple_go_gin_gorm_postgres_be_template/db"
 	cfg "simple_go_gin_gorm_postgres_be_template/internal/config"
-	"simple_go_gin_gorm_postgres_be_template/internal/models"
 
 	"github.com/joho/godotenv"
 )
-
-var ModelsRegistry = []any{
-	&models.Blogs{},
-	&models.CultureBlog{},
-	&models.Demographies{},
-	&models.Galleries{},
-	&models.Geographies{},
-	&models.Images{},
-	&models.IndustriesBlog{},
-	&models.Officials{},
-	&models.Roles{},
-	&models.ShopsAndUmkmsBlog{},
-	&models.Superadmins{},
-	&models.Timelines{},
-}
 
 // create all tables
 func MigrateUp() {
@@ -29,12 +14,14 @@ func MigrateUp() {
 	godotenv.Load()
 
 	cfg.ConnectDB()
+	defer cfg.DisconnectDB()
+
 	DB := cfg.DB
 	if DB == nil {
 		log.Fatal("❌ Failed to migrate up, database is not connected yet")
 	}
 
-	err := DB.AutoMigrate(ModelsRegistry...)
+	err := DB.AutoMigrate(db.ModelsRegistry...)
 	if err != nil {
 		log.Fatal("❌ Failed to do database migration : ", err)
 	}
@@ -46,13 +33,16 @@ func MigrateUp() {
 func MigrateDown() {
 	log.Println("ℹ️ Starting database reset (migrate down)")
 	godotenv.Load()
+
 	cfg.ConnectDB()
+	defer cfg.DisconnectDB()
+
 	DB := cfg.DB
 	if DB == nil {
 		log.Fatal("❌ Failed to migrate down, database is not connected yet")
 	}
 
-	err := DB.Migrator().DropTable(ModelsRegistry...)
+	err := DB.Migrator().DropTable(db.ModelsRegistry...)
 	if err != nil {
 		log.Fatal("❌ Failed to drop table: ", err)
 	}
