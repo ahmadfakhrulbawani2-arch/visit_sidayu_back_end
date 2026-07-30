cfdace9d085c5d4bf3309fde4f9c862d11e50585
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2026-07-30 09:15:06 +0700

    fix: no undefined type fix

--- internal/constants/input/InputConfig.go
+++ internal/constants/input/InputConfig.go
@@ -1,6 +1,6 @@
 package input
 
 var (
-	Area_Units               = []string{"km2", "ha", "-"}
-	Population_Density_Units = []string{"jiwa/km2", "jiwa/ha", "-"}
+	Area_Units               = []string{"km2", "ha"}
+	Population_Density_Units = []string{"jiwa/km2", "jiwa/ha"}
 )
