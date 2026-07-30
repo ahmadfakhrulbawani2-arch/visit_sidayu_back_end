a3d1493889f245fa62902a1505450edb72c411d4
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2026-07-30 08:37:04 +0700

    feat: add validation func

---
+++ internal/helpers/validation/parse_input.go
@@ -0,0 +1,19 @@
+package validation
+
+import (
+	"net/http"
+	myE "visit-sidayu-backend/internal/constants/errorss"
+	hp "visit-sidayu-backend/internal/helpers"
+
+	"github.com/gin-gonic/gin"
+)
+
+// ParseInputJSON melakukan binding JSON dan otomatis menangani error response
+func ParseInputJSON[T any](ctx *gin.Context) (*T, error) {
+	var input T
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		hp.RespError(ctx, http.StatusBadRequest, myE.Msg400Err, err)
+		return nil, err
+	}
+	return &input, nil
+}
