d3393cf796d0e59ca4408839cf38e077d96b7783
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2026-08-04 11:23:44 +0700

    feat: add cors configuration constants

---
+++ internal/constants/corscfg/corsconfig.go
@@ -0,0 +1,16 @@
+package corscfg
+
+var(
+	AllowedOrigins = []string{
+		"http://localhost:3000",
+		"https://visit-sidayu-front-end.vercel.app",
+	}
+	AllowedMethods = []string{
+		"GET",
+		"POST",
+		"PUT",
+		"PATCH",
+		"DELETE",
+		"OPTIONS",
+	}
+)
