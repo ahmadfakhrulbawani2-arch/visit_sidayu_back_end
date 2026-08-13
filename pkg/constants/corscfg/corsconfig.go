8e9f2818c9b3e8935ff2e42db07a3fc024f2c39e
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2026-08-13 13:58:43 +0700

    update: add more cors yikes

--- pkg/constants/corscfg/corsconfig.go
+++ pkg/constants/corscfg/corsconfig.go
@@ -4,6 +4,8 @@ var(
 	AllowedOrigins = []string{
 		"http://localhost:3000",
 		"http://localhost:3001",
+		"http://127.0.0.1:3000",
+		"http://127.0.0.1:3001",
 		"http://10.73.51.153:3000",
 		"http://10.73.51.153:3001",
 		"http://192.168.1.9:3000",
